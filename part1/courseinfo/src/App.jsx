d0e43bc623c9c70c35bff1237a23630a480cb27c
Nityum <nitvfx@gmail.com>
2025-08-22 23:19:52 -0400

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,25 +4,24 @@ const Header = () => {
  )
 }
 
+const Part = (props) => {
+  return (
+    <>
+    <p>
+      {props.title} {props.exercises}
+    </p>
+    </>
+  )
+}
+
 const Content = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return(
     <>
-    <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p></>
+    <Part title='Fundamentals of React' exercises='10' />
+    <Part title='Using props to pass data' exercises='7'/>
+    <Part title='State of a component' exercises='14'/>
+    </>
   )
 }
 

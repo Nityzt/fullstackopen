7eaa7f2231e304f4a5dc4333f2910528a09827e9
Nityum <nitvfx@gmail.com>
2025-08-22 23:00:19 -0400

    exercise 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = () => {
+ return (
+   <h1>'Half Stack application development'</h1>
+ )
+}
+
+const Content = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return(
+    <>
+    <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p></>
+  )
+}
+
+const Total = () => {
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+return(
+  <p>
+    {exercises1 + exercises2 + exercises3}
+  </p>
+  
+)
+}
+
+const App = () => {
+  
+  
+
+  return (
+
+    <div>
+      <Header />
+      <Content />
+      <Total/>
+    </div>
+  )
+}
+
+export default App

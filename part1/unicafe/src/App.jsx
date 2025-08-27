a78cf4692ac8a72cba6d20a738b22b02f70a9bf3
Nityzt <nitvfx@gmail.com>
2025-08-27 01:57:59 -0400

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,16 +5,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return (
+  const total = good + bad + neutral
+
+  const average = total > 0 ? (good*1 + neutral*0 + bad*-1)/total : 0
+
+  const positive = total>0 ? (good/total)*100 : 0
+
+    return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={()=>{setGood(good+1), console.log(good+1)}}>good</button>
+      <button onClick={()=>{setGood(good+1)}}>good</button>
       <button onClick={()=>{setNeutral(neutral+1)}}>neutral</button>
       <button onClick={()=>{setBad(bad+1)}}>bad</button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good {good}
+        <br></br>
+        neutral {neutral}
+        <br></br>
+        bad {bad}
+        <br></br>
+        all {total}
+        <br></br>
+        average {average}
+        <br></br>
+        positive {positive} %</p>
+
     </div>
   )
 }

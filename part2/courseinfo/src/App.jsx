d9e4db5878285fb94bf551cda84b79650d7d6c5a
Nityum <nitvfx@gmail.com>
2025-09-03 10:55:31 -0400

    exercise 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,37 +1,10 @@
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4         
-      }
-    ]
-  }
-
+ const Course = ({course}) => {
   const part = course.parts.map((part)=> <p key={part.id}>{part.name} {part.exercises}</p>)
+
   const total = course.parts.reduce((sum, part)=> {
     return sum + part.exercises  
   },0)
 
-  const Course = ({course}) => {
     return (
       <>
        <h1>
@@ -40,12 +13,60 @@ const App = () => {
       <>{part}</>
       <b>total of {total} exercises</b>
       </>
-     
-      
     )
   }
 
-  return <Course course={course} />
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <>
+    {courses.map(course => <Course key={course.id} course={course}/>)}
+    </>
+  )
 }
 
 export default App
